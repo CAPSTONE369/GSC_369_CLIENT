a5537cf845ac9b8df9516e8ba16f8de372469e5d
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-02-12 00:46:27 +0900

    💄 style: Modify Size Of Modal

--- lib/src/widgets/register/modal/food_add_modal.dart
+++ lib/src/widgets/register/modal/food_add_modal.dart
@@ -27,60 +27,65 @@ class _FoodAddModalState extends State<FoodAddModal> {
   Widget build(BuildContext context) {
     FoodNotifier foodNotifier = Provider.of<FoodNotifier>(context);
     return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       title: const Text('New Food!'),
-      content: Column(children: [
-        TextFormField(
-          decoration: InputDecoration(
-            fillColor: Colors.white,
-            filled: true,
-            labelText: 'Food Name',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
+      content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                labelText: 'Food Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+              ),
+              initialValue: '',
+              onSaved: (value) {
+                _name = value!;
+              },
             ),
-            floatingLabelBehavior: FloatingLabelBehavior.never,
-          ),
-          initialValue: '',
-          onSaved: (value) {
-            _name = value!;
-          },
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-            fillColor: Colors.white,
-            filled: true,
-            labelText: 'Quantity',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
+            TextFormField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                labelText: 'Quantity',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+              ),
+              initialValue: '',
+              onSaved: (value) {
+                _quantity = value!;
+              },
             ),
-            floatingLabelBehavior: FloatingLabelBehavior.never,
-          ),
-          initialValue: '',
-          onSaved: (value) {
-            _quantity = value!;
-          },
-        ),
-        DropdownButton<String>(
-            value: sortingValue,
-            icon: const Icon(Icons.arrow_downward),
-            elevation: 16,
-            style: const TextStyle(color: Colors.deepPurple),
-            underline: Container(
-              height: 2,
-              color: Colors.deepPurpleAccent,
-            ),
-            onChanged: (String? value) {
-              // This is called when the user selects an item.
-              setState(() {
-                sortingValue = value!;
-              });
-            },
-            items: sortingList.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList()),
-      ]),
+            DropdownButton<String>(
+                value: sortingValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    sortingValue = value!;
+                  });
+                },
+                items:
+                    sortingList.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList()),
+          ]),
       actions: <Widget>[
         TextButton(
           onPressed: () => Navigator.pop(context, 'Cancel'),
