dc876074c2e37a2719918afd6a91c7a4a54c7bde
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-03-02 10:16:41 +0900

    💄 style: add FoodRegisterCard Style

--- lib/src/widgets/register/card/register_card.dart
+++ lib/src/widgets/register/card/register_card.dart
@@ -1,6 +1,7 @@
 import 'package:client/src/model/food.dart';
 import 'package:flutter/material.dart';
 import 'package:client/src/widgets/register/card/textbox.dart';
+import 'package:intl/intl.dart';
 
 class FoodCard extends StatelessWidget {
   final Food food;
@@ -11,43 +12,69 @@ class FoodCard extends StatelessWidget {
     return Padding(
         padding: EdgeInsets.all(5),
         child: Container(
-          decoration: BoxDecoration(
-              color: const Color(0xffF3F4FB),
-              borderRadius: BorderRadius.circular(8)),
-          height: 100,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+            decoration: BoxDecoration(
+                color: const Color(0xffF3F4FB),
+                borderRadius: BorderRadius.circular(8)),
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  flex: 15,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      RegisterTextBox(text: food.name),
-                      RegisterTextBox(text: food.quantity)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          RegisterTextBox(
+                            text: food.name,
+                            value: 10,
+                            size: 140,
+                          ),
+                          RegisterTextBox(
+                            text: DateFormat('yyyy-MM-dd').format(food.date),
+                            value: 10,
+                            size: 120,
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          RegisterTextBox(
+                            text: food.quantity,
+                            value: 10,
+                            size: 80,
+                          ),
+                          RegisterTextBox(
+                            text: food.isFrozen,
+                            value: 10,
+                            size: 80,
+                          ),
+                          RegisterTextBox(
+                            text: food.classification,
+                            value: 10,
+                            size: 80,
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      RegisterTextBox(text: food.isFrozen),
-                      RegisterTextBox(text: food.classification)
-                    ],
-                  )
-                ],
-              )
-            ],
-          ),
-        ));
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: VerticalDivider(
+                      thickness: 1, width: 1, color: Colors.black),
+                ),
+                Flexible(
+                    flex: 3,
+                    fit: FlexFit.tight,
+                    child: IconButton(
+                        onPressed: () => {},
+                        icon: const Icon(Icons.delete_forever_outlined)))
+              ],
+            )));
   }
 }
 
--- lib/src/widgets/register/card/textbox.dart
+++ lib/src/widgets/register/card/textbox.dart
@@ -4,16 +4,28 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 class RegisterTextBox extends StatelessWidget {
   final String text;
-  const RegisterTextBox({super.key, required this.text});
+  final int value;
+  final double size;
+  const RegisterTextBox(
+      {super.key, required this.text, required this.value, required this.size});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(3),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 20, backgroundColor: Colors.white),
-      ),
-    );
+    return Flexible(
+        flex: value,
+        child: Container(
+          alignment: Alignment.center,
+          height: 40,
+          width: size,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          child: Padding(
+            padding: EdgeInsets.all(5),
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ));
   }
 }
