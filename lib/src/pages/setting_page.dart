56e5ebd114afb42a80ae8bd36148bd17cc5c751b
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-03-15 03:39:58 +0900

    💄 ui: add Layout of Setting Page

--- lib/src/pages/setting_page.dart
+++ lib/src/pages/setting_page.dart
@@ -12,8 +12,55 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Setting Page"),
+    return Scaffold(
+      appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: const Text(
+            "Setting",
+            style: TextStyle(
+              color: const Color(0xff395BA9),
+            ),
+          )),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            margin:
+                const EdgeInsets.only(top: 70, bottom: 40, left: 10, right: 10),
+            alignment: Alignment.center,
+            height: 200,
+            child: const Text("sunnyineverywhere@ewhain.net"),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: const Color(0xff395BA9),
+                  width: 5,
+                )),
+          ),
+          Expanded(
+              child: SizedBox(
+            height: 500,
+            child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: (context, idx) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Text Button',
+                        style: TextStyle(
+                            color: Color(0xff395BA9),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  );
+                }),
+          ))
+        ],
+      )),
     );
   }
 }
