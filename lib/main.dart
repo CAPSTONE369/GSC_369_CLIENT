937393e85a7626cf3012b90d0e130a76367e5fa7
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-03-17 00:15:48 +0900

    💄 ui: add Landing Page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:client/src/controller/food_controller.dart';
 import 'package:client/src/pages/login_page.dart';
 import 'package:client/src/pages/fridge_page.dart';
 import 'package:client/src/pages/route/main_page.dart';
+import 'package:client/src/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
@@ -24,9 +25,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Speaking Fridge",
+      theme: ThemeData(
+        fontFamily: 'Noto_Serif_KR',
+      ),
       initialRoute: "/",
       routes: {
-        '/': (context) => const LogInPage(),
+        '/': (context) => const SplashPage(),
+        '/login': (context) => const LogInPage(),
         '/main': (context) => const MainPage()
       },
     );
---
+++ lib/src/pages/splash_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late AnimationController controller;
+  static final storage = FlutterSecureStorage();
+  dynamic accessToken = '';
+  dynamic refreshToken = '';
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      /// [AnimationController]s can be created with `vsync: this` because of
+      /// [TickerProviderStateMixin].
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat(reverse: true);
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _asyncMethod();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  _asyncMethod() async {
+    accessToken = await storage.read(key: 'acessToken');
+    refreshToken = await storage.read(key: 'refreshToken');
+    if (accessToken != null && refreshToken != null) {
+      Navigator.pushNamed(context, '/main');
+    } else {
+      Navigator.pushNamed(context, '/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color(0xff395BA9),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  "Welcome! \nNow We're Loading . . .",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 24),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                LinearProgressIndicator(
+                  value: controller.value,
+                  semanticsLabel: 'Linear progress indicator',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
